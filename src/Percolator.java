a1e1fb09ca2c3deac178a880839f679f446f3016
Nathan <Nathan@DESKTOP-GN3TL23>
2022-11-03 17:20:28 -0400

    Added getGroup Image is now created when getImage is called Added JavaDoc comments

--- src/Percolator.java
+++ src/Percolator.java
@@ -4,6 +4,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.Hashtable;
 import java.util.LinkedList;
@@ -17,14 +18,32 @@ import javax.swing.SwingConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+/**
+ * A 2 dimensional grid where connections in cardinal directions have a random double from 0-1 associated.
+ * Connections with a value less than the set probability are active.
+ * Cells that have connections are part of the same group.
+ * 
+ * @author Nathaniel Dunlap
+ * @version 11/03/2022
+ *
+ */
 public class Percolator {
 
     private int width, height;
     private double probability;
     private double[][] edges;
+    private int[] pixels;
     private int[] colors;
     private BufferedImage img;
     
+    /**
+     * Creates a Percolator with the given size and probability using the provided Random.
+     * 
+     * @param width width of the Percolator
+     * @param height height of the Percolator
+     * @param probability the probability that any given connection is active
+     * @param rand the Random that will be used to construct the Percolator
+     */
     public Percolator(int width, int height, double probability,  Random rand) {
         this.width = width;
         this.height = height;
@@ -40,6 +59,8 @@ public class Percolator {
                     edges[i + (j + 1) * width][3] = edges[i + j * width][1];
             }
         
+        pixels = new int[edges.length];
+        
         colors = new int[edges.length];
         for(int i = 0; i < colors.length; i++)
             colors[i] = rand.nextInt(0x1000000);
@@ -49,41 +70,76 @@ public class Percolator {
         setProbability(probability);
     }
     
+    /**
+     * Creates a Percolator with the given size and probability.
+     * 
+     * @param width width of the Percolator
+     * @param height height of the Percolator
+     * @param probability the probability that any given connection is active
+     */
     public Percolator(int width, int height, double probability) {
         this(width, height, probability, new Random());
     }
     
+    /**
+     * Creates a Percolator with the given size with a probability of 0 using the provided Random.
+     * 
+     * @param width width of the Percolator
+     * @param height height of the Percolator
+     * @param rand the Random that will be used to construct the Percolator
+     */
     public Percolator(int width, int height, Random rand) {
         this(width, height, 0, rand);
     }
     
+    /**
+     * Creates a Percolator with the given size.
+     * 
+     * @param width width of the Percolator
+     * @param height height of the Percolator
+     */
     public Percolator(int width, int height) {
         this(width, height, 0);
     }
     
+    /**
+     * Returns the width of the Percolator.
+     * @return the width of the Percolator
+     */
     public int getWidth() {
         return width;
     }
     
+    /**
+     * Returns the height of the Percolator.
+     * @return the height of the Percolator
+     */
     public int getHeight() {
         return height;
     }
     
+    /**
+     * Returns the probability that any given connection is active.
+     * @return the probability that any given connection is active
+     */
     public double getProbability() {
         return probability;
     }
     
+    /**
+     * Sets the probability that any given connection is active and finds which cells are connected.
+     * @param probability the probability that any given connection is active
+     */
     public void setProbability(double probability) {
         this.probability = probability;
         boolean check[] = new boolean[width * height];
-        int[] pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
         for (int i = 0; i < width * height; i++)
             if (!check[i]) {
                 Deque<Integer> stack = new LinkedList<>();
                 stack.push(i);
                 while (!stack.isEmpty()) {
                     int n = stack.pop();
-                    pixels[n] = colors[i];
+                    pixels[n] = i;
                     check[n] = true;
                     double arr[] = edges[n];
                     for (int j = 0; j < 4; j++) {
@@ -95,10 +151,34 @@ public class Percolator {
             }
     }
     
+    /**
+     * Returns an int representing which group the given cell is a part of.
+     * The int returned is arbitrary. The only guarantee is that cells in the same group will return the same int and cells in different groups will return different ints.
+     * 
+     * @param x
+     * @param y
+     * @return
+     */
+    public int getGroup(int x, int y) {
+        return pixels[x+y*width];
+    }
+    
+    /**
+     * Creates an image representing the Percolator, where groups are defined by random colors.
+     * @return an image representing the Percolator
+     */
     public BufferedImage getImage() {
+        Arrays.setAll(((DataBufferInt)img.getRaster().getDataBuffer()).getData(), n -> colors[pixels[n]]);
         return img;
     }
     
+	/**
+	 * Creates a JFrame with a slider that can be used to adjust the probability of the Percolator.
+	 * Up to three arguments may be used specifying, in order, the width, height, and random seed of the Percolator.
+	 * By default a width of 800, a height of 600, and a random seed is used.
+	 * 
+	 * @param args optional arguments specifying width, height, and a seed
+	 */
 	public static void main(String[] args) {
 		int width = args.length >= 2 ? Integer.parseInt(args[0]) : 800,
 				height = args.length >= 2 ? Integer.parseInt(args[1]) : 600;
